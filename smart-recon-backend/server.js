088ec2fdb781b00616cd7116f7c7f3f96f5399f8
full-StackDevelopers <rasithshahul@gmail.com>
2026-02-03 15:52:12 +0530

    Link

--- smart-recon-backend/server.js
+++ smart-recon-backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const app = require("./src/app");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,3 +12,8 @@ mongoose
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.log(err));
+app.use(
+  cors({
+    origin: "https://smart-audit-frontend.onrender.com",
+  }),
+);
--- smart-recon-frontend/src/services/api.js
+++ smart-recon-frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
+<<<<<<< HEAD
   baseURL: "https://smart-audit-backend.onrender.com",
+=======
+  baseURL: process.env.REACT_APP_API_URL,
+>>>>>>> 2ceded2 (Link)
 });
 
 api.interceptors.request.use((config) => {
